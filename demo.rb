cf9e43cbb696d3769fa2e1f86f5f7fb0b7a3a60d
Victor Wang <wjp2013@gmail.com>
2018-02-20 21:53:17 +0800

    添加变量

--- demo.rb
+++ demo.rb
@@ -1,8 +1,11 @@
 #!/usr/bin/env ruby
 require 'csv'
 require 'benchmark'
+require 'progress_bar'
 require './account'
 require './strategy'
+
+bar = ProgressBar.new
 
 # 初始化虚拟账户状态
 def initialize_account
@@ -10,13 +13,13 @@ def initialize_account
 end
 
 # 初始化策略
-def initialize_strategy(risk, avg_range)
-  @strategy = Strategy.new(risk, avg_range)
+def initialize_strategy(risk, avg_range, refresh_rate)
+  @strategy = Strategy.new(risk, avg_range, refresh_rate)
 end
 
-def loopback_testing(risk, avg_range)
+def loopback_testing(risk, avg_range, refresh_rate)
   initialize_account
-  initialize_strategy(risk, avg_range)
+  initialize_strategy(risk, avg_range, refresh_rate)
 
   CSV.foreach('data.csv', headers: true) do |row|
     @account.buy row['open'].to_f, 1000 if @strategy.can_buy?
@@ -34,20 +37,34 @@ def clearance
 end
 
 def display_testing_result
-  puts "Money: #{@account.usdt_balance} Risk: #{@strategy.risk} Avg_range: #{@strategy.avg_range}"
+  if @account.usdt_balance > Account::CAPITAL_BASE
+    puts "Money: #{@account.usdt_balance} Risk: #{@strategy.risk} Avg_range: #{@strategy.avg_range}"
+  end
 end
 
 # Plan 1: 买入价系数从 0.095 到 1.02 之间
 # function_time = Benchmark.realtime do
+#   n = 0
 #   (0.95...1.02).step(0.001).each do |risk|
-#     loopback_testing(risk)
+#     n += 1
+#     loopback_testing(risk, 6, 6)
+#   end
+#   puts "共测试 #{n} 个组合"
+# end
+
+# Plan 2：平均值计算范围从 5min 到 120min
+# function_time = Benchmark.realtime do
+#   (5...120).each do |avg_range|
+#     loopback_testing(1.0041, avg_range)
 #   end
 # end
 
-# Plan 2：平均值计算范围从 1x5min 到 12x5min
+# Plan 3：高频交易每分钟1次，组合买入价系数 从 1.0041 到 1.02 之间，平均值计算范围从 5min 到 10min，每隔 5 分钟交易
 function_time = Benchmark.realtime do
-  (2...12).each do |avg_range|
-    loopback_testing(1.0041, avg_range)
+  (1.0041...1.2).step(0.001).each do |risk|
+    (5...10).each do |avg_range|
+      loopback_testing(risk, avg_range, 5)
+    end
   end
 end
 
--- strategy.rb
+++ strategy.rb
@@ -1,41 +1,50 @@
 class Strategy
-  attr_accessor :average_price, :last_price, :close_price_history, :risk, :avg_range
+  attr_accessor :average_price, :last_price, :close_price_history, :refresh_rate_counter
+  attr_accessor :risk, :avg_range, :refresh_rate
 
-  def initialize(risk, avg_range)
+  def initialize(risk, avg_range, refresh_rate)
     self.average_price = nil
     self.last_price = nil
+    self.refresh_rate_counter = 0
     self.close_price_history = []
+
     self.risk = risk
     self.avg_range = avg_range
+    self.refresh_rate = refresh_rate
   end
 
   def can_buy?
     return false unless average_price
-    last_price > risk * average_price
+    last_price > risk * average_price if refresh_rate_counter = refresh_rate
   end
 
   def can_sell?
     return false unless average_price
-    last_price < average_price
+    last_price < average_price if refresh_rate_counter = refresh_rate
   end
 
   def calculate_average_price
     return if close_price_history.size < avg_range
     self.average_price = close_price_history.sum / close_price_history.size
-    puts "calculate_average_price #{average_price}"
   end
 
   def clear_close_price_history
     until close_price_history.size <= avg_range do
       self.close_price_history = close_price_history.yield_self { |ary| ary.shift; ary }
-      puts "clear_close_price_history #{close_price_history}"
     end
+  end
+
+  def reset_refresh_rate_counter
+    self.refresh_rate_counter = 0 if refresh_rate_counter > refresh_rate
   end
 
   def upgrade(close_price, open_price)
     self.close_price_history << close_price
     self.last_price = open_price
 
+    self.refresh_rate_counter += 1
+
+    reset_refresh_rate_counter
     clear_close_price_history
     calculate_average_price
   end
