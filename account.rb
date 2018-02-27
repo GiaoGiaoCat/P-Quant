27f0098591ee93054b4b1fae616bb6f6cdaa22a1
Victor Wang <wjp2013@gmail.com>
2018-02-27 21:36:35 +0800

    今天数据

--- account.rb
+++ account.rb
@@ -10,26 +10,17 @@ class Account
   end
 
   def buy(strategy)
-    # return if usdt_balance <= strategy.last_price.to_f * 1000 * 1.002
     return if usdt_balance <= strategy.last_price.to_f * 1.002
     amount =
       if coin_balance == 0
-        ((usdt_balance / 10) / strategy.last_price.to_f).to_i
+        ((usdt_balance / 10) / strategy.last_price.to_f)
       else
-        ((usdt_balance / 20) / strategy.last_price.to_f).to_i
+        ((usdt_balance / 20) / strategy.last_price.to_f)
       end
     self.usdt_balance -= strategy.last_price.to_f * amount * 1.002
     self.coin_balance += amount
     log strategy, "In", amount
   end
-
-  # def sell(strategy)
-  #   return if coin_balance <= 1000
-  #   cache_coin_balance = coin_balance
-  #   self.usdt_balance += strategy.last_price.to_f * coin_balance * 0.998
-  #   self.coin_balance -= 1000
-  #   log strategy, "Out", cache_coin_balance
-  # end
 
   def clearance(strategy)
     return if coin_balance == 0
@@ -42,7 +33,7 @@ class Account
   def log(strategy, type, count = 1000)
     price = strategy.last_price
     time = DateTime.strptime(strategy.time,'%s').to_s
-    #account_book.puts "#{time},#{type},#{strategy.average_price_1.round(5)},#{strategy.average_price_2.round(5)},#{price},#{count},#{(price * count).round(5)},#{(price * count * 0.002).round(4)},#{usdt_balance.round(5)},#{coin_balance},#{usdt_balance+(price*coin_balance)}"
+    account_book.puts "#{time},#{type},#{strategy.average_price_1.round(5)},#{strategy.average_price_2.round(5)},#{price},#{count.round(4)},#{(price * count).round(5)},#{(price * count * 0.002).round(4)},#{usdt_balance.round(5)},#{coin_balance.round(5)},#{(usdt_balance+(price*coin_balance)).round(5)}"
   end
 
 end
--- demo.rb
+++ demo.rb
@@ -16,11 +16,11 @@ def initialize_strategy(risk, avg_range_1, avg_range_2, refresh_rate)
   @strategy = StrategyBak.new(risk, avg_range_1, avg_range_2, refresh_rate)
 end
 
-def loopback_testing(account_book, risk, avg_range_1, avg_range_2, refresh_rate)
+def loopback_testing(account_book, risk, avg_range_1, avg_range_2, refresh_rate, data_file)
   initialize_account(account_book)
   initialize_strategy(risk, avg_range_1, avg_range_2, refresh_rate)
 
-  CSV.foreach('data.csv', headers: true) do |row|
+  CSV.foreach(data_file, headers: true) do |row|
     @strategy.upgrade row['time'], row['close'].to_f, row['open'].to_f
     @account.buy @strategy if @strategy.can_buy?
     # @account.sell row['open'].to_f, 1000 if @strategy.can_sell?
@@ -36,14 +36,12 @@ def clearance
 end
 
 def display_testing_result
-  # return false if @account.usdt_balance < Account::CAPITAL_BASE
-  # "Money: #{@account.usdt_balance.round(2)} Risk: #{@strategy.risk} Avg_range_1: #{@strategy.avg_range_1},Avg_range_2: #{@strategy.avg_range_2}"
-  if @account.usdt_balance/Account::CAPITAL_BASE > 1.05
+  if @account.usdt_balance > Account::CAPITAL_BASE
     puts "#{@account.usdt_balance.round(2)},#{@strategy.risk},#{@strategy.avg_range_1},#{@strategy.avg_range_2}"
   end
 end
 
-#Plan 1: 买入：P>Avg(n)，卖出P<Avg(n)，for n from 5 to 120
+# Plan 1: 买入：P>Avg(n)，卖出P<Avg(n)，for n from 5 to 120
 # risks = (0.99..1.06).step(0.01).to_a.each do |risk|
 #   avg_range_2 = (5..2400).step(15).to_a.each do |avg_range_2|
 #     avg_range_1 = (90..7500).step(15).to_a.each do |avg_range_1|
@@ -56,47 +54,15 @@ end
 #   end
 #  end
 
-
-# puts "Parent #{Process.pid}"
-# risks = (0.9..1.02).step(0.01).to_a.each do |risk|
-#   fork do
-#     File.open("plan_b/#{risk}.csv", 'a+') do |file|
-#       avg_range_2 = (5..2400).step(15).to_a.each do |avg_range_2|
-#         avg_range_1 = (90..7500).step(15).to_a.each do |avg_range_1|
-#           result = loopback_testing(@account, risk, avg_range_1, avg_range_2, 1)
-#           if @account.usdt_balance > Account::CAPITAL_BASE
-#             file.puts result
-#           end
-#         end
-#       end
-#     end
-#     puts "Child1 #{Process.pid}: #{range1.to_a.index(number)}"
-#   end
-# end
-# Process.wait
-
-puts "Parent #{Process.pid}"
-fork do
-  avg_range_2 = (5..1200).step(15).to_a.each do |avg_range_2|
-    avg_range_1 = (90..7500).step(15).to_a.each do |avg_range_1|
-      loopback_testing(nil, 0.98, avg_range_1, avg_range_2, 1)
-    end
-  end
-end
-fork do
-  avg_range_2 = (1200..2400).step(15).to_a.each do |avg_range_2|
-    avg_range_1 = (90..7500).step(15).to_a.each do |avg_range_1|
-      loopback_testing(nil, 0.98, avg_range_1, avg_range_2, 1)
+# NOTE: 2月27日 策略
+# 买入：P>avg(n) and P>avg(m), m < n, 卖出：P<avg(m)
+%w[btc dta eth iost].each do |data_file|
+  fork do
+    File.open("log/#{data_file}_log.csv", 'w+') do |file|
+      puts "Working on #{data_file}"
+      file.puts '时间,交易类型,平均价1,平均价2,当前交易价,交易数量,交易金额,手续费,USDT,COIN,Net_Value'
+      loopback_testing(file, 0.98, 3000, 20, 1, "data/#{data_file}.csv")
     end
   end
 end
 Process.wait
-
-# end
-
-# Plan 1: 买入：P>avg(n) and P>avg(m), m < n, 卖出：P<avg(m)
-#File.open("demo2.csv", 'w+') do |file|
-#  file.puts '时间,交易类型,平均价1,平均价2,当前交易价,交易数量,交易金额,手续费,USDT,COIN,Net_Value'
-  #loopback_testing(@account, 0.98, 450, 50, 1)
-#end
-puts "end"
